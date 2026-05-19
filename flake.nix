e88947df0af9dd191deb2cfe9138fea51acff081
quest4ione <quest4ione@gmail.com>
2026-05-19 21:25:11 +0200

    modularize hosts

--- flake.nix
+++ flake.nix
@@ -2,25 +2,21 @@
   description = "My NixOS flake configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-    in {
-      nixosConfigurations = {
-        quest-laptop = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-          ];
-          specialArgs = {
-            inherit pkgs-unstable;
-          };
+  outputs = { nixpkgs-stable, nixpkgs-unstable, ... }@inputs: {
+    nixosConfigurations = {
+      hp-pavilion = nixpkgs-stable.lib.nixosSystem {
+        modules = [
+          ./hosts/hp-pavilion
+        ];
+
+        specialArgs = {
+          inherit inputs;
         };
       };
     };
+  };
 }
