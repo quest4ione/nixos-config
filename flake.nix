d0d8732b09a904affdac4d17c6c330442938c1d7
quest4ione <quest4ione@gmail.com>
2026-06-02 16:12:27 +0200

    add home-manager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
   };
 
   outputs = { nixpkgs-stable, nixpkgs-unstable, ... }@inputs: {
--- hosts/hp-pavilion/default.nix
+++ hosts/hp-pavilion/default.nix
@@ -4,6 +4,14 @@
   in {
     imports = [
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { inherit inputs; };
+        home-manager.users = {
+          quest = ../../homes/quest.nix;
+        };
+      }
     ];
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
