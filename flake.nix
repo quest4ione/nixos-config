5f611c85f41be35a9af657bb0da58397d7fc524b
quest4ione <quest4ione@gmail.com>
2026-06-11 09:28:37 +0200

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -2,23 +2,28 @@
   description = "My NixOS flake configuration";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager/release-26.05";
   };
 
-  outputs = { nixpkgs-stable, nixpkgs-unstable, ... }@inputs: {
-    nixosConfigurations = {
-      hp-pavilion = nixpkgs-stable.lib.nixosSystem {
-        modules = [
-          ./hosts/hp-pavilion
-        ];
+  outputs = (inputs@{ flake-parts, ... }: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
+        nixosConfigurations = {
+          hp-pavilion = inputs.nixpkgs-stable.lib.nixosSystem {
+            modules = [
+              ./hosts/hp-pavilion
+            ];
 
-        specialArgs = {
-          inherit inputs;
+            specialArgs = {
+              inherit inputs;
+            };
+          };
         };
       };
-    };
-  };
+    }
+  );
 }
