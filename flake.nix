5cae803b6605138611a574c42187b0daa4f5138c
quest4ione <quest4ione@gmail.com>
2026-06-12 20:50:37 +0200

    make wrapper-modules follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     import-tree.url = "github:denful/import-tree";
 
     home-manager = {
